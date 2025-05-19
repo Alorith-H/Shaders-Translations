3412fc7b4d074372612bcaf321804bcfdbb1caca
Alorith-H <2835147611@qq.com>
2025-05-19 20:48:14 +0800

    修改代码

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
+from foo import DocProcessing
+
 #光影文件路径
 Shaders_File: str = "Str"
 
 print("Shaders-Translations")
 Shaders_File = input("光影文件地址：")
 print("*光影包文件地址为：" + Shaders_File)
-print("*复制光影包文件到程序目录")
+DocProcessing.print_func(Shaders_File)
---
+++ foo/DocProcessing.py
@@ -0,0 +1,4 @@
+def print_func(path):
+    print("*复制光影包文件到程序目录")
+    print("Hello :", path)
+    return

470c47930b70f2399dcb509ae6a329c2830ded2b
Alorith-H <2835147611@qq.com>
2025-05-19 21:12:42 +0800

    新增文件检查，检查是否存在所选文件

--- Main.py
+++ Main.py
@@ -6,4 +6,4 @@ Shaders_File: str = "Str"
 print("Shaders-Translations")
 Shaders_File = input("光影文件地址：")
 print("*光影包文件地址为：" + Shaders_File)
-DocProcessing.print_func(Shaders_File)
+DocProcessing.Move_func(Shaders_File)
--- foo/DocProcessing.py
+++ foo/DocProcessing.py
@@ -1,4 +1,27 @@
-def print_func(path):
-    print("*复制光影包文件到程序目录")
-    print("Hello :", path)
-    return
+#Shutil函数实现文件复制操作
+import shutil
+#os函数
+import os
+
+def check_file_path(path):
+    if not os.path.exists(path):
+        print("找不到文件")
+        return 0
+    return 1
+
+
+def Move_func(path):
+    path = path.replace('\\','/')
+    if check_file_path(path):
+        #获取光影文件名
+        Shaders_name = path.split("\\")[-1].replace('"', "")
+        print(Shaders_name)
+        # 定义源文件和目标文件的路径
+        src_path = path
+        dst_path = "./Temp/"
+        # 使用shutil模块复制文件
+        print("*复制光影包文件到程序目录")
+        shutil.copy(src_path, dst_path)
+        print("Hello :", path)
+    else:
+        pass
